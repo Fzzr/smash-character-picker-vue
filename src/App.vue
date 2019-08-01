ff8839de4c98fa31cd69c3d74a0e91584ae5c6b6
Ben Milman <bpmilman@gmail.com>
2019-07-31 18:15:47 -0700

    Display character list with toggleable enable/disable states

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="app">
+    <SelectButton/>
+    <CharacterPanel
+      v-bind:characterList=characterList
+    />
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
+import CharacterPanel from './components/CharacterPanel.vue';
 
 export default {
   name: 'app',
-}
+  components: {
+    CharacterPanel,
+  },
+  computed: mapState([
+    "characterList"
+  ])
+};
 </script>
 
 <style>
