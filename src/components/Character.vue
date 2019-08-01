38a35e3b7bcb363eb858d8669f891f5744b5d8d9
Ben Milman <bpmilman@gmail.com>
2019-07-31 18:14:48 -0700

    Add Character and CharacterPanel components

---
+++ src/components/Character.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class='character'
+    v-bind:class="[{selected: selected}]"
+    v-bind:index="index"
+    v-bind:selected="selected"
+    :tabindex="index"
+    v-on:click="$emit('toggleCharacter', id)"
+    v-on:keypress.enter="$emit('toggleCharacter', id)">
+    <img
+      v-bind:image="image"
+      v-bind:name="name"
+      :alt="name"
+      :src="`characters/${image}`"
+    />
+    <h3>{{ name }}</h3>
+    <div
+      v-bind:disabled="disabled"
+      v-bind:class="[{disabled: disabled}]"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Character',
+  props: {
+    disabled: Boolean,
+    image: String,
+    index: Number,
+    id: String,
+    name: String,
+    selected: Boolean
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: auto;
+}
+
+.character {
+  margin: 2px;
+  position: relative;
+  width: 150px;
+}
+
+.disabled {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
+  opacity: 60%;
+  background-color: rgba(169, 169, 169, .5);
+}
+
+.selected {
+  outline: solid red;
+}
+</style>
---
+++ src/components/CharacterPanel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="character-panel">
+    <character
+      v-for="(character, index) in characterList"
+      v-bind:characterList="characterList"
+      v-bind:character="character"
+      v-bind:disabled="character.disabled"
+      v-bind:id="character.id"
+      v-bind:image="character.image"
+      v-bind:index="index+1"
+      v-bind:key="character.id"
+      v-bind:name="character.name"
+      v-bind:selected="character.selected"
+      v-on:toggleCharacter="toggleCharacter"
+    />
+  </div>
+</template>
+
+<script>
+import Character from './Character';
+
+export default {
+  name: 'CharacterPanel',
+  props: {
+    characterList: Array
+  },
+  components: {
+    Character
+  },
+  methods: {
+    toggleCharacter: function(characterId) {
+      this.$store.commit('toggleCharacter', { characterId });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.character-panel {
+  outline: solid grey;
+  margin: 5px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+</style>
+
