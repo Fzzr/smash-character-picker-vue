c4995cbc4a1c8bcdfc5b7101ba4bcf7140bf5d81
Ben Milman <bpmilman@gmail.com>
2019-08-15 13:07:14 -0700

    convert remaining linting warnings in character constants

--- src/constants/characters.ts
+++ src/constants/characters.ts
@@ -4,538 +4,538 @@ export const CHARACTER_LIST = [
     id: 'mario',
     image: 'mario.png',
     name: 'MARIO',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'donkey-kong',
     image: 'donkey-kong.png',
     name: 'DONKEY KONG',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'link',
     image: 'link.png',
     name: 'LINK',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'samus',
     image: 'samus.png',
     name: 'SAMUS',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'dark-samus',
     image: 'dark-samus.png',
     name: 'DARK SAMUS',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'yoshi',
     image: 'yoshi.png',
     name: 'YOSHI',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'kirby',
     image: 'kirby.png',
     name: 'KIRBY',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'fox',
     image: 'fox.png',
     name: 'FOX',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'pikachu',
     image: 'pikachu.png',
     name: 'PIKACHU',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'luigi',
     image: 'luigi.png',
     name: 'LUIGI',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ness',
     image: 'ness.png',
     name: 'NESS',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'captain-falcon',
     image: 'captain-falcon.png',
     name: 'CAPTAIN FALCON',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'jigglypuff',
     image: 'jigglypuff.png',
     name: 'JIGGLYPUFF',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'peach',
     image: 'peach.png',
     name: 'PEACH',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'daisy',
     image: 'daisy.png',
     name: 'DAISY',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'bowser',
     image: 'bowser.png',
     name: 'BOWSER',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ice-climbers',
     image: 'ice-climbers.png',
     name: 'ICE CLIMBERS',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'sheik',
     image: 'sheik.png',
     name: 'SHEIK',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'zelda',
     image: 'zelda.png',
     name: 'ZELDA',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'dr-mario',
     image: 'dr-mario.png',
     name: 'DR. MARIO',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'pichu',
     image: 'pichu.png',
     name: 'PICHU',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'falco',
     image: 'falco.png',
     name: 'FALCO',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'marth',
     image: 'marth.png',
     name: 'MARTH',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'lucina',
     image: 'lucina.png',
     name: 'LUCINA',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'young-link',
     image: 'young-link.png',
     name: 'YOUNG LINK',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ganondorf',
     image: 'ganondorf.png',
     name: 'GANONDORF',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'mewtwo',
     image: 'mewtwo.png',
     name: 'MEWTWO',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'roy',
     image: 'roy.png',
     name: 'ROY',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'chrom',
     image: 'chrom.png',
     name: 'CHROM',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'mr-game-and-watch',
     image: 'mr-game-and-watch.png',
     name: 'MR. GAME AND WATCH',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'meta-knight',
     image: 'meta-knight.png',
     name: 'META KNIGHT',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'pit',
     image: 'pit.png',
     name: 'PIT',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'dark-pit',
     image: 'dark-pit.png',
     name: 'DARK PIT',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'zero-suit-samus',
     image: 'zero-suit-samus.png',
     name: 'ZERO SUIT SAMUS',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'wario',
     image: 'wario.png',
     name: 'WARIO',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'snake',
     image: 'snake.png',
     name: 'SNAKE',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ike',
     image: 'ike.png',
     name: 'IKE',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'pokemon-trainer',
     image: 'pokemon-trainer.png',
     name: 'POKEMON TRAINER',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'diddy-kong',
     image: 'diddy-kong.png',
     name: 'DIDDY KONG',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'lucas',
     image: 'lucas.png',
     name: 'LUCAS',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'sonic',
     image: 'sonic.png',
     name: 'SONIC',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'king-dedede',
     image: 'king-dedede.png',
     name: 'KING DEDEDE',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'olimar',
     image: 'olimar.png',
     name: 'OLIMAR',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'lucario',
     image: 'lucario.png',
     name: 'LUCARIO',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'rob',
     image: 'rob.png',
     name: 'R.O.B.',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'toon-link',
     image: 'toon-link.png',
     name: 'TOON LINK',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'wolf',
     image: 'wolf.png',
     name: 'WOLF',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'villager',
     image: 'villager.png',
     name: 'VILLAGER',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'megaman',
     image: 'megaman.png',
     name: 'MEGAMAN',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'wii-fit-trainer',
     image: 'wii-fit-trainer.png',
     name: 'WII FIT TRAINER',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'rosalina',
     image: 'rosalina.png',
     name: 'ROSALINA & LUMA',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'little-mac',
     image: 'little-mac.png',
     name: 'LITTLE MAC',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'greninja',
     image: 'greninja.png',
     name: 'GRENINJA',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'palutena',
     image: 'palutena.png',
     name: 'PALUTENA',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'pac-man',
     image: 'pac-man.png',
     name: 'PAC-MAN',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'robin',
     image: 'robin.png',
     name: 'ROBIN',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'shulk',
     image: 'shulk.png',
     name: 'SHULK',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'bowser-jr',
     image: 'bowser-jr.png',
     name: 'BOWSER JR.',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'duck-hunt',
     image: 'duck-hunt.png',
     name: 'DUCK HUNT',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ryu',
     image: 'ryu.png',
     name: 'RYU',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ken',
     image: 'ken.png',
     name: 'KEN',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'cloud',
     image: 'cloud.png',
     name: 'CLOUD',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'corrin',
     image: 'corrin.png',
     name: 'CORRIN',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'bayonetta',
     image: 'bayonetta.png',
     name: 'BAYONETTA',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'inkling',
     image: 'inkling.png',
     name: 'INKLING',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'ridley',
     image: 'ridley.png',
     name: 'RIDLEY',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'simon',
     image: 'simon.png',
     name: 'SIMON',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'richter',
     image: 'richter.png',
     name: 'RICHTER',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'king-k-rool',
     image: 'king-k-rool.png',
     name: 'KING K. ROOL',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'isabelle',
     image: 'isabelle.png',
     name: 'ISABELLE',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'incineroar',
     image: 'incineroar.png',
     name: 'INCINEROAR',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'piranha-plant',
     image: 'piranha-plant.png',
     name: 'PIRANHA PLANT',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'joker',
     image: 'placeholder.png',
     name: 'JOKER',
-    selected: false
+    selected: false,
   },
   {
     disabled: false,
     id: 'hero',
     image: 'placeholder.png',
     name: 'HERO',
-    selected: false
+    selected: false,
   },
   {
     disabled: true,
     id: 'mii-brawler',
     image: 'mii-brawler.png',
     name: 'MII BRAWLER',
-    selected: false
+    selected: false,
   },
   {
     disabled: true,
     id: 'mii-swordfighter',
     image: 'mii-swordfighter.png',
     name: 'MII SWORDFIGHTER',
-    selected: false
+    selected: false,
   },
   {
     disabled: true,
     id: 'mii-gunner',
     image: 'mii-gunner.png',
     name: 'MII GUNNER',
-    selected: false
+    selected: false,
   },
 ];
